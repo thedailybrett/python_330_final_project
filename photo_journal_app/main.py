a36f65d8b7ea1fb6bd96ca710b791eb20dce497d
brettdaily <brett.n.daily@gmail.com>
2025-06-18 23:55:33 -0700

    fixes

--- photo_journal_app/main.py
+++ photo_journal_app/main.py
@@ -11,8 +11,9 @@ from jinja2_fragments.fastapi import Jinja2Blocks
 from PIL import Image
 from tinydb import TinyDB, Query
 
-# photo_journal_app/
+# cd photo_journal_app
 # tailwindcss -i static/src/tw.css -o static/css/styles.css --watch
+# cd photo_journal_app
 # uvicorn main:app --reload
 
 app = FastAPI()
@@ -69,9 +70,12 @@ async def post_photo(request: Request, entry: Annotated[str, Form()], photo_uplo
     if valid_image_file:
         resize_image_for_web(photo_file_path)
         uploaded_at = time.strftime("%m/%d/%Y %I:%M:%S%p")
-        db.insert({"entry": entry,
+        photo_id = db.insert({"entry": entry,
                    "file_path": photo_file_path,
                    "uploaded_at": uploaded_at})
+        photo = db.get(doc_id=photo_id)
+        photo["doc_id"] = photo_id
+        db.update(photo, doc_ids=[photo_id])
     sorted_photos = get_sorted_photos(db.all(), 0, PHOTOS_PER_PAGE)
     context = {
         "request": request,
@@ -82,9 +86,15 @@ async def post_photo(request: Request, entry: Annotated[str, Form()], photo_uplo
     return templates.TemplateResponse(name="photo_journal.html.jinja2", context=context, block_name="photos")
 
 @app.get("/edit-photo", response_class=HTMLResponse)
-async def get_edit_photo_form(request: Request, photo_id: int, db: TinyDB = Depends(get_db)):
+async def get_edit_photo_form(request: Request,
+                              photo_id: int,
+                              db: TinyDB = Depends(get_db)
+                              ):
     Photo = Query()
-    photo = db.get(Photo.doc_id == photo_id)
+    photo = db.get(doc_id=photo_id)
+    if photo is None:
+        raise ValueError(f"No photo found with doc_id {photo_id}")
+    photo["doc_id"] = photo_id
     template = templates.get_template("edit_photo_form.html.jinja2")
     return HTMLResponse(template.render(request=request, photo=photo))
 
@@ -105,6 +115,7 @@ async def edit_photo(request: Request,
 @app.delete("/delete-photo")
 async def delete_photo(photo_id: int,
                        db: TinyDB = Depends(get_db)):
+    Photo = Query() # --?
     photo = db.get(doc_id=photo_id)
     if photo:
         file_path = photo.get("file_path", "")
@@ -118,12 +129,18 @@ async def delete_photo(photo_id: int,
     return Response(status_code=200)
 
 @app.get("/load-photos", response_class=HTMLResponse)
-async def load_photos(request: Request, photo_count: int, db: TinyDB = Depends(get_db)):
+async def load_photos(request: Request,
+                      photo_count: int,
+                      db: TinyDB = Depends(get_db)
+                      ):
+    total_photos = len(db.all())
     new_photo_count = photo_count + PHOTOS_PER_PAGE
     sorted_photos = get_sorted_photos(db.all(), 0, new_photo_count)
+    has_more_photos = new_photo_count < total_photos
     context = {
         "request": request,
         "photos": sorted_photos,
-        "photo_count": new_photo_count
+        "photo_count": new_photo_count,
+        "has_more_photos": has_more_photos,
         }
     return templates.TemplateResponse(name="photo_journal.html.jinja2", context=context, block_name="photos")
